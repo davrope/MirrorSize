a5d6c12df42a519f265a6ee6ba60b3c3def7472f
David <rodpe.david@gmail.com>
2022-05-02 22:27:56 -0500

    Cleaning unnecesary code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
-
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import reducers from './reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import './App.css';
 
 
